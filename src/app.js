167c63a36c6f20c08505c6255910b01bf3e70086
pablo escalante <56638479+PabloEscalante2000@users.noreply.github.com>
2024-03-08 21:42:15 -0500

    cambios en el cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {pool} from './db.js';
+import cors from 'cors';
 import tareasRoutes from './routes/tareas.routes.js';
 import "./config.js"
 
@@ -7,6 +8,7 @@ import "./config.js"
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 //* Prueba de query
 app.get("/ping", async (req,res)=>{
--- src/controllers/tareas.controller.js
+++ src/controllers/tareas.controller.js
@@ -75,7 +75,5 @@ export const deleteTareas = async(req,res)=>{
         return res.status(500).json({
             message:"Something goes wrong"
         })
-    }
-
-    
+    }    
 }
